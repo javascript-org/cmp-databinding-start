89e142e94553aac7e058ee2190e91d1c3ad261ad
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2019-11-14 16:01:19 +0530

    @Input('alias') decorator

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
   // use @Input so that the parent component can access this property
-  @Input() element: { type: string, name: string, content: string }
+  @Input('srvElement') element: { type: string, name: string, content: string }
   constructor() { }
 
   ngOnInit() {
